dd2e6e4787573cdb943099e60acefd67add2bbea
sirjandrn25 <56628702+sirjandrn25@users.noreply.github.com>
2023-12-11 23:10:53 +0545

    make query api

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 
 import pandas as pd
 import json
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Union
 
 
 # Dependency
-
-
-
 app = FastAPI()
 origins = [
     "http://localhost:3000",
@@ -25,11 +22,13 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
-file_url = './employee_demo.csv'
+demo_file_url =  "https://csv-visualizer.s3.us-east-1.amazonaws.com/employee_demo.csv-8239"
 @app.get('/file-table-info')
-def visualize_csv_file():
+def visualize_csv_file(q: Union[str, None] = demo_file_url):
+
+
     try:
-        df = pd.read_csv(file_url)
+        df = pd.read_csv(q)
         table_form = json.loads(df.to_json(orient='table', index=False))
         return table_form
     except Exception as e:
@@ -37,9 +36,9 @@ def visualize_csv_file():
         return e
  
 @app.get('/file-meta-info')
-def visualize_csv_file():
+def visualize_csv_file(q: Union[str, None] = demo_file_url):
     try:
-        df = pd.read_csv(file_url)
+        df = pd.read_csv(q)
         meta_info = df.describe().to_json()
         return json.loads(meta_info)
     except Exception as e:
